9daa05960a08cc35b645b8499f67e2cc47c399af
Caberdrill56 <dominik.rossner14@googlemail.com>
2024-09-04 18:49:19 +0200

    Fist Build

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import pygame
 from constants import *
 
 def main():
-    print(f"Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    running = True
+    pygame.init()
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)).fill("Black")
+        pygame.display.flip()
+
+
 
 if __name__ == "__main__":
     main()
